be0e744a274835e49c5e261dfb51c703df0781fe
Jari Leskinen <jarileskinen@icloud.com>
2014-03-17 15:06:23 +0100

    Init commit

---
+++ chat.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define BUFSIZE 10000
+
+typedef struct addrinfo addrinfo;
+
+int main(int argc, const char *argv[])
+{
+  addrinfo hints, *res;
+  int status, sockfd, maxfd;
+  char buf[BUFSIZE];
+  fd_set masterrfs, masterwfs, readfs, writefs;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: chat hostname port\n");
+    return 1;
+  }
+
+  FD_ZERO(&masterrfs);
+  FD_ZERO(&masterwfs);
+  FD_ZERO(&readfs);
+  FD_ZERO(&writefs);
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return 2;
+  }
+
+  if ((sockfd = socket(res->ai_family, res->ai_socktype,
+                       res->ai_protocol)) == -1) {
+      perror("Socket error");
+      return 2;
+  }
+
+  if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+    perror("Unable to connect");
+    return 2;
+  }
+
+  /*while (fgets(buf, BUFSIZE, stdin) != NULL) {
+    send(sockfd, buf, BUFSIZE, 0);
+    recv(sockfd, buf, BUFSIZE, 0);
+    printf("%s\n", buf);
+  }*/
+
+  FD_SET(STDIN_FILENO, &masterrfs);
+  FD_SET(sockfd, &masterrfs);
+
+  maxfd = sockfd;
+
+  while (1) {
+    readfs = masterrfs;
+    writefs = masterwfs;
+
+    if (select(maxfd+1, &readfs, NULL, NULL, 0) == -1) {
+      perror("select");
+      return 1;
+    }
+    printf("Select done\n");
+
+    if (FD_ISSET(STDIN_FILENO, &readfs)) {
+      fgets(buf, BUFSIZE, stdin);
+      send(sockfd, buf, BUFSIZE, 0);
+    }
+    if (FD_ISSET(sockfd, &readfs)) {
+      recv(sockfd, buf, BUFSIZE, 0);
+      printf("%s\n", buf);
+    }
+  }
+
+  freeaddrinfo(res);
+  close(sockfd);
+
+  return 0;
+}
---
+++ server.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define MYPORT "3490"
+#define BACKLOG 10
+#define BUFSIZE 10000
+
+typedef struct sockaddr_storage sockaddr_storage;
+typedef struct sockaddr sockaddr;
+typedef struct addrinfo addrinfo;
+
+void *get_in_addr(sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char const *argv[]) {
+  sockaddr_storage remoteaddr;
+  socklen_t addrlen;
+  addrinfo hints, *p;
+  int status, newfd, listener, fdmax, nbytes;
+  char buf[BUFSIZE];
+  fd_set master;
+  fd_set readfs;
+  char remoteIP[INET6_ADDRSTRLEN];
+
+  int i, j;
+
+  FD_ZERO(&master);
+  FD_ZERO(&readfs);
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if ((status = getaddrinfo(NULL, MYPORT, &hints, &p)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return 2;
+  }
+
+  if ((listener = socket(p->ai_family, p->ai_socktype,
+                       p->ai_protocol)) == -1) {
+      perror("Socket error");
+      return 2;
+  }
+
+  if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
+    perror("Bind error");
+    return 2;
+  }
+
+  /*freeaddrinfo(ai);*/
+
+  if (listen(listener, BACKLOG) == -1) {
+    perror("Listen error");
+    return 2;
+  }
+  printf("Now listening on port %s\n", MYPORT);
+
+  FD_SET(listener, &master);
+
+  fdmax = listener;
+
+  while (1) {
+    readfs = master;
+    if (select(fdmax+1, &readfs, NULL, NULL, NULL) == -1) {
+      perror("select");
+      exit(4);
+    }
+    
+    for (i = 0; i <= fdmax; ++i) {
+      if (FD_ISSET(i, &readfs)) {
+        if (i == listener) {
+          addrlen = sizeof remoteaddr;
+          newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
+          printf("%s\n", "Connected!");
+
+          if (newfd == -1) {
+            perror("Accept");
+          } else {
+            FD_SET(newfd, &master);
+            if (newfd > fdmax) {
+              fdmax = newfd;
+            }
+            printf("Selectserver: new connection from %s on "
+                "socket %d\n", inet_ntop(remoteaddr.ss_family,
+                  get_in_addr((struct sockaddr*)&remoteaddr),
+                  remoteIP, INET6_ADDRSTRLEN),
+                newfd);
+          }
+        } else {
+          if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+            if (nbytes == 0) {
+              printf("selectserver: socket %d hung up\n", i);
+            } else {
+              perror("recv");
+            }
+            close(i);
+            FD_CLR(i, &master);
+          } else {
+            for (j = 0; j <= fdmax; j++) {
+              if (FD_ISSET(j, &master)) {
+                if (j != listener && j != i) {
+                  if (send(j, buf, nbytes, 0) == -1) {
+                    perror("send");
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /*while (1) {
+    addr_size = sizeof their_addr;
+    if ((newfd = accept(listener, (sockaddr *)&their_addr, &addr_size)) == -1) {
+      perror("Accept error");
+      return 2;
+    }
+    printf("Connection recieved!\n");
+
+    if (!fork()) {
+      close(listener);
+      recv(newfd, buf, BUFSIZE, 0);
+      send(newfd, buf, BUFSIZE, 0);
+      close(newfd);
+      exit(0);
+    }
+    close(newfd);
+  }*/
+
+  return 0;
+}
