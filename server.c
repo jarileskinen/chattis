c7c6d7773cc1faeb3ddadef8da5e33063dc784fc
Jari Leskinen <jarileskinen@icloud.com>
2014-03-19 23:10:18 +0100

    Restructure

--- server.c
+++ server.c
@@ -24,17 +24,16 @@ void *get_in_addr(sockaddr *sa) {
   return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+int setup_socket(addrinfo *hints, int *listener);
+int accept_connection(int listener);
+void close_connection(int fd, int nbytes);
+
 int main(int argc, char const *argv[]) {
-  sockaddr_storage remoteaddr;
-  socklen_t addrlen;
-  addrinfo hints, *p;
-  int status, newfd, listener, fdmax, nbytes;
+  addrinfo hints;
+  int listener, fdmax, nbytes;
   char buf[BUFSIZE];
   fd_set master;
   fd_set readfs;
-  char remoteIP[INET6_ADDRSTRLEN];
-
-  int i, j;
 
   FD_ZERO(&master);
   FD_ZERO(&readfs);
@@ -44,21 +43,7 @@ int main(int argc, char const *argv[]) {
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  if ((status = getaddrinfo(NULL, MYPORT, &hints, &p)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    return 2;
-  }
-
-  if ((listener = socket(p->ai_family, p->ai_socktype,
-                       p->ai_protocol)) == -1) {
-      perror("Socket error");
-      return 2;
-  }
-
-  if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
-    perror("Bind error");
-    return 2;
-  }
+  setup_socket(&hints, &listener); 
 
   /*freeaddrinfo(ai);*/
 
@@ -73,50 +58,36 @@ int main(int argc, char const *argv[]) {
   fdmax = listener;
 
   while (1) {
+    int i;
+
     readfs = master;
     if (select(fdmax+1, &readfs, NULL, NULL, NULL) == -1) {
       perror("select");
       exit(4);
     }
-    
-    for (i = 0; i <= fdmax; ++i) {
-      if (FD_ISSET(i, &readfs)) {
-        if (i == listener) {
-          addrlen = sizeof remoteaddr;
-          newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
-          printf("%s\n", "Connected!");
 
-          if (newfd == -1) {
-            perror("Accept");
-          } else {
-            FD_SET(newfd, &master);
-            if (newfd > fdmax) {
-              fdmax = newfd;
-            }
-            printf("Selectserver: new connection from %s on "
-                "socket %d\n", inet_ntop(remoteaddr.ss_family,
-                  get_in_addr((struct sockaddr*)&remoteaddr),
-                  remoteIP, INET6_ADDRSTRLEN),
-                newfd);
-          }
+    for (i = 0; i <= fdmax; ++i) {
+      if (!FD_ISSET(i, &readfs)) continue;
+
+      if (i == listener) {
+        int newfd;
+        if (!(newfd = accept_connection(listener))) continue;
+
+        FD_SET(newfd, &master);
+        fdmax = newfd > fdmax ? newfd : fdmax;
+      } else {
+        if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+          close_connection(i, nbytes);
+          FD_CLR(i, &master);
         } else {
-          if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
-            if (nbytes == 0) {
-              printf("selectserver: socket %d hung up\n", i);
-            } else {
-              perror("recv");
-            }
-            close(i);
-            FD_CLR(i, &master);
-          } else {
-            for (j = 0; j <= fdmax; j++) {
-              if (FD_ISSET(j, &master)) {
-                if (j != listener && j != i) {
-                  if (send(j, buf, nbytes, 0) == -1) {
-                    perror("send");
-                  }
-                }
-              }
+          int j;
+          for (j = 0; j <= fdmax; j++) {
+            if (!FD_ISSET(j, &master)) continue;
+            if (j == listener) continue;
+            if (j == i) continue;
+            
+            if (send(j, buf, nbytes, 0) == -1) {
+              perror("send");
             }
           }
         }
@@ -125,4 +96,61 @@ int main(int argc, char const *argv[]) {
   }
 
   return 0;
+}
+
+void init() {
+
+}
+
+int setup_socket(addrinfo *hints, int *listener) {
+  addrinfo *p;
+  int status;
+
+  if ((status = getaddrinfo(NULL, MYPORT, hints, &p)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    exit(2);
+  }
+
+  if ((*listener = socket(p->ai_family, p->ai_socktype,
+                       p->ai_protocol)) == -1) {
+    perror("Socket error");
+    exit(2);
+  }
+
+  if (bind(*listener, p->ai_addr, p->ai_addrlen) == -1) {
+    perror("Bind error");
+    exit(2);
+  }
+
+  return 1;
+}
+
+int accept_connection(int listener) {
+  int newfd;
+  socklen_t addrlen;
+  sockaddr_storage remoteaddr;
+  char remoteIP[INET6_ADDRSTRLEN];
+  
+  addrlen = sizeof(remoteaddr);
+  newfd = accept(listener,(sockaddr*)&remoteaddr, &addrlen);
+  if (newfd == -1) {
+    perror("Accept");
+    return 0;
+  }
+ 
+  printf("New connection from %s on "
+            "socket %d\n", inet_ntop(remoteaddr.ss_family,
+            get_in_addr((sockaddr*)&remoteaddr),
+            remoteIP, INET6_ADDRSTRLEN),
+            newfd);
+  return newfd;
+}
+
+void close_connection(int fd, int nbytes) {
+  if (nbytes == 0) {
+    printf("Closed socket %d", fd);
+  } else {
+    perror("recv");
+  }
+  close(fd);
 }
